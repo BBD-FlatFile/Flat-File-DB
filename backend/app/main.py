fe6ecd0fdd81fce97da83fae5824e57b3cca2876
ShaariqBBD <shaariq.patel@bbd.co.za>
2024-07-23 19:46:19 +0200

    Added s3 sdk, updated calls, added reconciliation, added some new requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.routers import transactions
+from app.routers import transactions, database
 
 app = FastAPI()
 app.include_router(transactions.router)
+app.include_router(database.router)
 
 
 @app.get("/")
